78c00dae48e1f98772df1bfb2107c7a04964a323
Tulasi Latha Gangadhari <65495688+TulasiLatha@users.noreply.github.com>
2023-04-09 23:04:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width,height){
+		this.width=width;
+		this.height=height;
+	}
+	getWidth(){
+		return this.width;
+	}
+	getHeight(){
+return this.height;
+	}
+	getArea(){
+		return width*height;
+	}
+}
 
-class Square extends Animal {}
+class Square extends Animal {
+	getPerimeter(){
+		return 2*(l+b);
+	}
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
